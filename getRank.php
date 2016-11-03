17adde199832d3b17f3cee8f285d3e14333408f2
ShanaMaid <uestczeng@gmail.com>
2016-11-03 16:28:32 +0800

    2016-11-03 16:28:32

--- getRank.php
+++ getRank.php
@@ -7,6 +7,11 @@
 		fwrite($fp_rank, "[".$str[$i]["cardTitle"][0]."](http://tieba.baidu.com/p/".$str[$i]["cardId"][0].")     ".$str[$i]["cardReply"][0]."回复           \r\n");
 	}
 
+	//  for ($i=0; $i < 100; $i++) { 
+	// 	fwrite($fp_rank, "排名：".($i+1)."\r\n\r\n 帖子名字：".$str[$i]["cardTitle"][0]."\r\n \r\n 帖子地址：http://tieba.baidu.com/p/".$str[$i]["cardId"][0]."\r\n\r\n     回复：".$str[$i]["cardReply"][0]."         \r\n");
+	// }
+
+
 	fclose($fp_rank);
 
 
